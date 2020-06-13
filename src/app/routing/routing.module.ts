7ed554fcef3d63959579073b378fff8e3381e8a9
Jeet Kishorkumar Poria <jporia77@in.ibm.com>
2020-06-13 20:48:04 +0530

    routing added

--- src/app/routing/routing.module.ts
+++ src/app/routing/routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes, Router } from '@angular/router';
 import { LoginComponent } from '../login/login.component';
 import { HomeComponent } from '../home/home.component';
 const routes: Routes = [
   {
     path : '',
+    component : LoginComponent
+  },
+  {
+    path : 'login',
     component : LoginComponent
   },
   {
@@ -19,6 +23,7 @@ const routes: Routes = [
     RouterModule.forRoot(routes)
     
   ],
+  exports : [RouterModule],
   declarations: []
 })
 export class RoutingModule { }
